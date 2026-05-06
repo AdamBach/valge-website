a5d47c42756abf9d6773ce79afb1180fdfdd0eb5
Neohuman <sk31239@gmail.com>
2026-05-07 07:39:46 +0900

    Remove unused auth boilerplate

--- src/app/auth/page.js
+++
@@ -1,9 +0,0 @@
-// Authentication Page (/auth)
-// This page handles user login and signup
-// Users are redirected here when they need to authenticate
-
-import AuthForm from '@/components/AuthForm'
- 
-export default function AuthPage() {
-  return <AuthForm />
-} 
